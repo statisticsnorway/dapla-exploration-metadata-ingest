053dd5e91bce3917d93196dc5a56446bd92d4bb5
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-05-18 12:46:32 +0200

    Added simple test to publish one message to topic.

---
+++ src/test/java/no/ssb/dapla/gsim_metadata_ingest/PubSubMessagePublisherTest.java
@@ -0,0 +1,24 @@
+package no.ssb.dapla.gsim_metadata_ingest;
+
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import no.ssb.pubsub.EmulatorPubSub;
+import no.ssb.pubsub.PubSub;
+import org.junit.jupiter.api.Test;
+
+public class PubSubMessagePublisherTest {
+
+    @Test
+    public void publishOneMessage() {
+        PubSub pubSub = new EmulatorPubSub("localhost", 8538);
+        Publisher publisher = pubSub.getPublisher("dapla", "gsim-metadata-1");
+        try {
+            publisher.publish(PubsubMessage.newBuilder()
+                    .setData(ByteString.copyFromUtf8("{\"test\":\"me\"}"))
+                    .build());
+        } finally {
+            publisher.shutdown();
+        }
+    }
+}
