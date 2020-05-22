f96d923e87be35d980333f81db7ee4f845397307
RuneLind <rulind@gmail.com>
2020-05-22 17:47:31 +0200

    Use full path as id

--- src/main/java/no/ssb/dapla/gsim_metadata_ingest/GsimLdsHttpProvider.java
+++ src/main/java/no/ssb/dapla/gsim_metadata_ingest/GsimLdsHttpProvider.java
@@ -20,13 +20,10 @@ public class GsimLdsHttpProvider implements PersistenceProvider {
     @Override
     public void save(IdentifiableArtefact identifiableArtefact) {
         String entityType = identifiableArtefact.getClass().getSimpleName();
-//        String path = String.format("/%s/%s", entityType, identifiableArtefact.getId());
-        // TODO: create a unique path
-        String path = String.format("/%s/%s", entityType, identifiableArtefact.getName());
-
+        String path = String.format("%s/%s", entityType, identifiableArtefact.getId());
         System.out.println("Will post to path: " + path);
         WebClientResponse response = gsimLdsWebClient.put()
-                .path("InstanceVariable/test")
+                .path(path)
                 .contentType(MediaType.APPLICATION_JSON)
                 .readTimeout(30, ChronoUnit.SECONDS)
                 .connectTimeout(30, ChronoUnit.SECONDS)
