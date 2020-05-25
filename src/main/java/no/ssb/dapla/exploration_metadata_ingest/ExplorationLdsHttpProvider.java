767c0e0871abb0c7797383a565659bd743f368fb
RuneLind <rulind@gmail.com>
2020-05-25 13:34:49 +0200

    Integration with dataset-doc
    Create gsim objects in exploration-lds from Metadata template

---
+++ src/main/java/no/ssb/dapla/exploration_metadata_ingest/ExplorationLdsHttpProvider.java
@@ -0,0 +1,44 @@
+package no.ssb.dapla.exploration_metadata_ingest;
+
+import io.helidon.common.http.Http;
+import io.helidon.common.http.MediaType;
+import io.helidon.webclient.WebClient;
+import io.helidon.webclient.WebClientResponse;
+import no.ssb.dapla.dataset.doc.model.gsim.IdentifiableArtefact;
+import no.ssb.dapla.dataset.doc.model.gsim.PersistenceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.temporal.ChronoUnit;
+
+public class ExplorationLdsHttpProvider implements PersistenceProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExplorationLdsHttpProvider.class);
+
+    private final WebClient gsimLdsWebClient;
+
+    public ExplorationLdsHttpProvider(WebClient gsimLdsWebClient) {
+        this.gsimLdsWebClient = gsimLdsWebClient;
+    }
+
+    @Override
+    public void save(IdentifiableArtefact identifiableArtefact) {
+        String path = String.format("%s/%s", identifiableArtefact.getGsimName(), identifiableArtefact.getId());
+        LOG.info("Will post to path: {}", path);
+        WebClientResponse response = gsimLdsWebClient.put()
+                .path(path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .readTimeout(30, ChronoUnit.SECONDS)
+                .connectTimeout(30, ChronoUnit.SECONDS)
+                .submit(identifiableArtefact)
+                .toCompletableFuture()
+                .join();
+
+        if (!Http.ResponseStatus.Family.SUCCESSFUL.equals(response.status().family())) {
+            // To se error message from lds
+            LOG.warn("Exploration LDS: {}", response.content().as(String.class).toCompletableFuture().join());
+            throw new RuntimeException(String.format("Got response code %d from Exploration LDS with reason: %s",
+                    response.status().code(), response.status().reasonPhrase()));
+        }
+    }
+}
