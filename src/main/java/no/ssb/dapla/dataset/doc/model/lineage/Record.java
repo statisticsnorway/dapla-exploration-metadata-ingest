36b3edff9534969c982df19e0781334e75651f7a
Rune Lind <ssb.rlind@gmail.com>
2021-04-15 14:47:37 +0200

    Include dataset doc files (#15)
    * Include dataset-doc pojo files and remove lib
    
    * Remove Json order and filter not needed
    
    * Add valuation to test.

---
+++ src/main/java/no/ssb/dapla/dataset/doc/model/lineage/Record.java
@@ -0,0 +1,10 @@
+package no.ssb.dapla.dataset.doc.model.lineage;
+
+public class Record extends Field {
+
+    public Record() {
+        super();
+        type = "structure"; // always structure for LogicalRecord
+    }
+
+}
